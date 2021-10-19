a4bd63ff53534a75ab7e78099c545ce2d2ae67ff
Wonjin Cho <cwonjin1@gmail.com>
2021-10-19 13:57:22 -0400

    add solution

---
+++ src/index.js
@@ -0,0 +1,48 @@
+
+symbols = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
+
+
+const romanToInt = (s) => {
+    value = 0;
+    for(let i = 0; i < s.length; i+=1){
+        symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value += symbols[s[i]]
+    }
+    return value
+};
+console.log(romanToInt("MCMXCVI"))
+console.log(romanToInt("MCMXCIV"))
+// console.log(romanToInt(""))
+
+
+
+
+//nums = [-2,1,-3,4,-1,2,1,-5,4]
+// function maxSubArray(nums) {
+//     if(nums.length === 1){
+//         return nums[0];
+//     }
+//     let maxValue = nums[0];
+//     let accNum = nums[0];
+    
+    
+//     for(let i=1; i<=nums.length; i++){
+//         let calc = Math.max(nums[i], accNum + nums[i])
+//         if (calc > maxValue) 
+//             maxValue = calc;
+//         accNum = calc       // set accNum as calc so I can iterate through more bigger accNum compared to the previous accNum
+//     }
+//     return maxValue;
+
+// };
+
+// console.log(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+// console.log(maxSubArray([1]))
+// console.log(maxSubArray([5,4,-1,7,8]))
